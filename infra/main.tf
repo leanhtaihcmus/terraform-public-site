02788829b246d69f6aa1f38baaad8dd429f27782
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-24 18:26:02 +0700

    AWS Credentials configured locally for Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -12,4 +12,6 @@ terraform {
 provider "aws" {
   profile = "default"
   region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -12,3 +12,15 @@ variable "private_cidr_blocks" {
   type = list(string)
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
+
+# Change it and don't push to git
+variable "aws_access_key" {
+  type = string
+  default = "AWSXXXXXX0978"
+}
+
+# Change it and don't push to git
+variable "aws_secret_key" {
+  type = string
+  default = "AULP0XXXXXXY7US9XXXXOP56JX"
+}
