8faa19fc3bad71b0750c4e633e4893b0a6e43bf5
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-27 20:59:06 +0700

    Update presentation-tier

--- presentation-tier/src/index.ts
+++ presentation-tier/src/index.ts
@@ -8,9 +8,49 @@ const APPLICATION_LOAD_BALANCER = process.env.APPLICATION_LOAD_BALANCER;
 const app = express();
 app.use(json());
 
-app.get('/', async (req, res) => {
-    axios.get('http://169.254.169.254/latest/meta-data/hostname').then(async (response) => {
-        const hostname = await response.data;
-        res.send(`Hello from ${hostname}`);
+app.get("/", async (req, res) => {
+  axios
+    .get("http://169.254.169.254/latest/meta-data/hostname", {
+      headers: { "Content-Type": "text/plain" },
+    })
+    .then((response) => {
+      const hostname = response.data;
+      res.send(`Hello from ${hostname}`);
+    })
+    .catch((error) => {
+      res.send(`Connection timeout!`);
     });
+});
+
+app.get("/init", async (req, res) => {
+  axios
+    .get(`http://${APPLICATION_LOAD_BALANCER}/init`)
+    .then((response) => {
+      const data = response.data;
+      res.send(data);
+    })
+    .catch((error) => {
+      res.send(`Connection timeout!`);
+    });
+});
+
+app.get("/users", async (req, res) => {
+  axios
+    .get(`http://${APPLICATION_LOAD_BALANCER}/users`)
+    .then((response) => {
+      const data = response.data;
+      res.send(data);
+    })
+    .catch((error) => {
+      res.send(`Connection timeout!`);
+    });
+});
+  
+// Custom 404 route not found handler
+app.use((req, res) => {
+  res.status(404).send("404 not found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
 });
