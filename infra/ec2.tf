5e64ad16b6b3bdd61454d54f646d065db7074a9b
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-28 22:28:24 +0700

    config ec2 launch_template to application-tier

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -103,3 +103,42 @@ resource "aws_launch_template" "presentation_tier" {
     aws_lb.application_tier
   ]
 }
+
+resource "aws_launch_template" "application_tier" {
+  name = "application_tier"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 8
+    }
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_ecr_connection.name
+  }
+
+  instance_type = "t2.nano"
+  image_id      = data.aws_ami.amazon_linux_2.id
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.application_tier.id]
+  }
+
+  user_data = base64encode(templatefile("./../user-data/user-data-application-tier.sh", {
+    rds_hostname  = module.rds.rds_address,
+    rds_username  = var.rds_db_admin,
+    rds_password  = var.rds_db_password,
+    rds_port      = 3306,
+    rds_db_name   = var.db_name
+    ecr_url       = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    ecr_repo_name = var.ecr_application_tier,
+    region        = var.region
+  }))
+
+  depends_on = [
+    aws_nat_gateway.gw
+  ]
+}
